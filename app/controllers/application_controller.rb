011fb1ae359d4eab5b1307407bd2cc8381c16d9c
Lixiang Ding <zhimadlx@gmail.com>
2016-08-06 12:15:02 +0800

    init commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "foo" && password == "bar"
+    end
+  end
+end
